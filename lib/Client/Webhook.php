551138f0cf62285acb1288488051937619e51186
adagiomedia <adagiomediarus@gmail.com>
2023-12-05 09:37:37 +0100

    PHP Lib renew
    Version 04.12.2023

--- lib/Client/Webhook.php
+++ lib/Client/Webhook.php
@@ -41,115 +41,30 @@ class Webhook extends AbstractClient
         }
     }
 
-    public function getLatestDeliveries(string $storeId, string $webhookId, string $count): \Coinsnap\Result\WebhookDeliveryList
-    {
-        $url = $this->getApiUrl() . ''.COINSNAP_SERVER_PATH.'/' . urlencode($storeId) . '/webhooks/' . urlencode($webhookId) . '/deliveries?count=' . $count;
-        $headers = $this->getRequestHeaders();
-        $method = 'GET';
-        $response = $this->getHttpClient()->request($method, $url, $headers);
+    
 
-        if ($response->getStatus() === 200) {
-            $data = json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
-            return new \Coinsnap\Result\WebhookDeliveryList($data);
-        } else {
-            throw $this->getExceptionByStatusCode($method, $url, $response);
+    public function createWebhook(string $storeId, string $url, ?array $specificEvents, ?string $secret): \Coinsnap\Result\WebhookCreated {
+        $data = ['url' => $url];
+        
+        if(isset($specificEvents) && count($specificEvents) > 0){
+            $data['events'] = $specificEvents;
         }
-    }
-
-    public function getDelivery(string $storeId, string $webhookId, string $deliveryId): \Coinsnap\Result\WebhookDelivery
-    {
-        $url = $this->getApiUrl() . ''.COINSNAP_SERVER_PATH.'/' . urlencode($storeId) . '/webhooks/' . urlencode($webhookId) . '/deliveries/' . urlencode($deliveryId);
-        $headers = $this->getRequestHeaders();
-        $method = 'GET';
-        $response = $this->getHttpClient()->request($method, $url, $headers);
-
-        if ($response->getStatus() === 200) {
-            $data = json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
-            return new \Coinsnap\Result\WebhookDelivery($data);
-        } else {
-            throw $this->getExceptionByStatusCode($method, $url, $response);
-        }
-    }
-
-    /**
-     * Get the delivery's request.
-     *
-     * The delivery's JSON request sent to the endpoint.
-     *
-     * @param string $storeId
-     * @param string $webhookId
-     * @param string $deliveryId
-     * @return string JSON request
-     */
-    public function getDeliveryRequest(string $storeId, string $webhookId, string $deliveryId): string
-    {
-        $url = $this->getApiUrl() . ''.COINSNAP_SERVER_PATH.'/' . urlencode($storeId) . '/webhooks/' . urlencode($webhookId) . '/deliveries/' . urlencode($deliveryId);
-        $headers = $this->getRequestHeaders();
-        $method = 'GET';
-        $response = $this->getHttpClient()->request($method, $url, $headers);
-
-        if ($response->getStatus() === 200) {
-            return $response->getBody();
-        } else {
-            throw $this->getExceptionByStatusCode($method, $url, $response);
-        }
-    }
-
-    /**
-     * Redeliver the delivery.
-     *
-     * @param string $storeId
-     * @param string $webhookId
-     * @param string $deliveryId
-     * @return string The new delivery id being broadcasted. (Broadcast happen asynchronously with this call)
-     */
-    public function redeliverDelivery(string $storeId, string $webhookId, string $deliveryId): string
-    {
-        $url = $this->getApiUrl() . ''.COINSNAP_SERVER_PATH.'/' . urlencode($storeId) . '/webhooks/' .
-                        urlencode($webhookId) . '/deliveries/' . urlencode($deliveryId) . '/redeliver';
-        $headers = $this->getRequestHeaders();
-        $method = 'POST';
-        $response = $this->getHttpClient()->request($method, $url, $headers);
-
-        if ($response->getStatus() === 200) {
-            return json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
-        } else {
-            throw $this->getExceptionByStatusCode($method, $url, $response);
-        }
-    }
-
-    public function createWebhook(string $storeId, string $url, ?array $specificEvents, ?string $secret): \Coinsnap\Result\WebhookCreated { //bool $enabled = true,bool $automaticRedelivery = true
-        $data = [
-            //'enabled' => $enabled,
-            //'automaticRedelivery' => $automaticRedelivery,
-            'url' => $url
-        ];
-		
-
-        if ($specificEvents === null) {
-            $data['authorizedEvents'] = ['everything' => true];
-        } 
-        elseif (count($specificEvents) === 0) {
-            throw new \InvalidArgumentException('Argument $specificEvents should be NULL or contains at least 1 item.');
-        } 
-        else {
-            $data['authorizedEvents'] = ['everything' => false,'specificEvents' => $specificEvents];
-        }
-
+        
         if ($secret === '') {
             throw new \InvalidArgumentException('Argument $secret should be NULL (let Server auto-generate a secret) or you should provide a long and safe secret string.');
         } 
         elseif ($secret !== null) $data['secret'] = $secret;
 
         $url = $this->getApiUrl() . ''.COINSNAP_SERVER_PATH.'/' . urlencode($storeId) . '/webhooks';
-		
         $headers = $this->getRequestHeaders();
+        
         $method = 'POST';
-        $response = $this->getHttpClient()->request($method, $url, $headers, json_encode($data, JSON_THROW_ON_ERROR));				
+        $response = $this->getHttpClient()->request($method, $url, $headers, json_encode($data, JSON_THROW_ON_ERROR));	
 
         if ($response->getStatus() === 200) {
             $data = json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
-            return new \Coinsnap\Result\WebhookCreated($data);
+            $result = new \Coinsnap\Result\WebhookCreated($data);
+            return $result;
         } else {
             throw $this->getExceptionByStatusCode($method, $url, $response);
         }
@@ -161,35 +76,18 @@ class Webhook extends AbstractClient
      * @return \Coinsnap\Result\Webhook
      * @throws \JsonException
      */
-    public function updateWebhook(
-        string $storeId,
-        string $url,
-        string $webhookId,
-        ?array $specificEvents,
-        bool $enabled = true,
-        bool $automaticRedelivery = true,
-        ?string $secret = null
-    ): \Coinsnap\Result\Webhook {
+    public function updateWebhook(string $storeId, string $url, string $webhookId, ?array $specificEvents, bool $enabled = true,?string $secret = null): \Coinsnap\Result\Webhook {
         $data = [
           'enabled' => $enabled,
-          'automaticRedelivery' => $automaticRedelivery,
           'url' => $url,
           'secret' => $secret
         ];
 
-        // Specific events or all.
-        if ($specificEvents === null) {
-            $data['authorizedEvents'] = [
-              'everything' => true
-            ];
-        } elseif (count($specificEvents) === 0) {
-            throw new \InvalidArgumentException('Argument $specificEvents should be NULL or contains at least 1 item.');
-        } else {
-            $data['authorizedEvents'] = [
-              'everything' => false,
-              'specificEvents' => $specificEvents
-            ];
+        if(isset($specificEvents) && count($specificEvents) > 0){
+            $data['events'] = $specificEvents;
         }
+        
+        
 
         $url = $this->getApiUrl() . ''.COINSNAP_SERVER_PATH.'/' . urlencode($storeId) . '/webhooks/' . urlencode($webhookId);
         $headers = $this->getRequestHeaders();
